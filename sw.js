fa0306806c7b127f741818fc8c1aec3f6227e201
jawad17-creat <soulaimana.jawad17@gmail.com>
2026-09-16 22:45:20 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nutrition-cache-v2';
+const CACHE_NAME = 'nutrition-cache-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
